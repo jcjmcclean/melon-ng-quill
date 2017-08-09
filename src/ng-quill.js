f6af6aa9e715add21f3005695c65e47b8a6ee5e8
James McClean <jcjmcclean@gmail.com>
2017-08-09 08:06:34 +0100

    Added support for horizontal rules

--- src/ng-quill.js
+++ src/ng-quill.js
@@ -177,6 +177,40 @@
 
         container.append($editorElem)
 
+        var Embed = Quill.import('blots/block/embed');
+
+        class Hr extends Embed {
+          static create(value) {
+            let node = super.create(value);
+            // give it some margin
+            node.setAttribute('style', "height:0px; margin-top:10px; margin-bottom:10px;");
+            return node;
+          }
+        }
+
+        Hr.blotName = 'hr'; //now you can use .ql-hr classname in your toolbar
+        Hr.className = 'my-hr';
+        Hr.tagName = 'hr';
+
+        var customHrHandler = function(){
+          // get the position of the cursor
+          var range = Quill.getSelection();
+          if (range) {
+            // insert the <hr> where the cursor is
+            Quill.insertEmbed(range.index,"hr","null")
+          }
+
+          console.log('customHrHandler');
+        }
+
+        Quill.register({
+          'formats/hr': Hr
+        });
+
+        config.modules.toolbar.handlers = {
+          'hr': customHrHandler
+        }
+
         editor = new Quill(editorElem, config)
 
         this.ready = true
